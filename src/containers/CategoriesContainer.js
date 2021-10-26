f5eca22a6a72da4ca125acfe8ef15265ff6d208c
yani82 <yani.iman1@gmail.com>
2021-10-26 00:25:31 -0400

    utilizing switch

--- src/containers/CategoriesContainer.js
+++ src/containers/CategoriesContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import {fetchCategories} from '../actions/fetchCategories'; 
 import Categories from '../components/Categories';
 import Category from '../components/Category';
@@ -15,11 +15,14 @@ class CategoriesContainer extends React.Component {
     render() {
         return (
             <div>
-                <Route path='/categories/new' component={CategoryInput}/>
-                <br/>
-                <Route path='/categories/:id' render={(routerProps) => <Category {...routerProps} categories={this.props.categories} /> } />
-                <Route exact path='/categories' render={(routerProps) => <Categories {...routerProps} categories={this.props.categories} /> } />
+                <switch>
+                    <Route path='/categories/new' component={CategoryInput}/>
+                    <br/>
+                    <Route path='/categories/:id' render={(routerProps) => <Category {...routerProps} categories={this.props.categories} /> } />
+                    <Route exact path='/categories' render={(routerProps) => <Categories {...routerProps} categories={this.props.categories} /> } />
+                </switch>
             </div>
+            // switch: returns the first path that matches, it makes sure that we only see the new route
         )
     }
 }
