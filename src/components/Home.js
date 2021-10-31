85c60dcb4a4db21d08c736da8fd68a826894b718
yani82 <yani.iman1@gmail.com>
2021-10-31 02:41:44 -0400

    need to fix positioning of text in /

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,20 @@ import {NavLink} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import image from "../images/6.jpg";
 
-function Home () {
+function Home (props) {
   return (
     
     <main>
         <img src={image} alt="Wau" className="absolute object-cover w-full h-full"  />
         <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-            <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Welcome to Malaysian Creations!</h1><br /><br /> 
-                <p className="text-5xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Introducing the beauty of a unique culture through it's many offerings.</p><br /><br /> 
-                        <br></br>
-                            <div>          
-                            <Button variant="contained" color="default"><NavLink to='/categories'>Browse</NavLink></Button>
-                            <Button variant="contained" color="default"><NavLink to='/categories/new'>Add New</NavLink></Button>
-                            </div>
+            <h1 className="text-6xl text-green-600 font-bold cursive leading-none lg:leading-snug home-name">Welcome to Malaysian Creations!</h1><br />
+            <br /> 
+                <h2 className="text-4xl text-green-600 font-bold cursive leading-none lg:leading-snug home-name">Introducing the beauty of a unique culture through it's many offerings.</h2><br /><br />
+                    <br></br>
+                        <div>          
+                        <Button variant="contained" color="default"><NavLink to='/categories'>Browse</NavLink></Button>
+                        <Button variant="contained" color="default"><NavLink to='/categories/new'>Add New</NavLink></Button>
+                        </div>
         </section>
     </main>
    
