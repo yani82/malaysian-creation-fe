5a3848f42226d62d7c6781d61ae8fd2786cc5d4b
yani82 <yani.iman1@gmail.com>
2021-12-11 23:00:54 -0500

    match the footer color with the navbar and try to center text

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <div>
+        <div>Designed and Programmed by <a href="https://yaniportfolio.netlify.app/" target="_blank" rel="noreferrer">Yani A.</a></div>
+        <div>©2021</div>
+        </div>
+    )
+}
+
+export default Footer;
