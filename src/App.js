aed11e3b2405d65ebb5c49b641fe08109735ae88
yani82 <yani.iman1@gmail.com>
2022-01-22 23:25:22 -0500

    remove ItemsListContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import {connect} from 'react-redux'; 
-import CategoriesContainer from './containers/CategoriesContainer'; 
-import NavBar from "./components/NavBar";
+import CategoriesContainer from './containers/CategoriesContainer';
+import NavBar from "./components/NavBar"; 
 import Footer from "./components/Footer";
 class App extends React.Component {
 
