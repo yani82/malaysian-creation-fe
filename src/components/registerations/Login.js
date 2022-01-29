2e512d648ae3e840ca0800f8dbb40b190123c1f0
yani82 <yani.iman1@gmail.com>
2022-01-29 04:44:32 -0500

    add eventListeners on form and button

--- src/components/registerations/Login.js
+++ src/components/registerations/Login.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+
+    handleChange = (e) => {
+
+    }
+
+    handleSubmit = (e) => {
+        
+    }
+
   render() {
     return (
     <div>
-
+        <form>
+            <input type="email" 
+            name="email"
+            placeholder="email..."
+            required onChange/>
+            <input type="password"
+            name="password"
+            placeholder="password..."
+            required onChange/>
+            <button onSubmit>Log In</button>
+        </form>
     </div>
     )
   }
